16f29869eb23b45a84544d50edb18b57b5e80398
anthonycope <anthonycope@mail.usf.edu>
2013-09-29 23:50:22 -0400

    Added a map structure. Key is the node, vector of ints are the neighbors of that node.

--- triadmpi.cpp
+++ triadmpi.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <map>
 #include "mpi.h"
 
 using namespace std;
@@ -37,37 +38,78 @@ return tokens;
 
 }
 
+vector <int>  getInput(string line)
+{
+    stringstream ss(line); 
+    string num;
+    string temp;
+    int i;
+
+    getline(ss, num, ' ');
+
+    vector<string> stringNodes = tokenize(num, "\t");
+    vector<int> intNodes;            
+
+    for (int i = 0; i < stringNodes.size(); i++)
+    {
+        //cout << stringNodes[i] << " ";
+        intNodes.push_back(atoi(stringNodes[i].c_str()));
+    }
+
+    return intNodes;
+}
+
 int main ()
 {
     fstream inputFile;
     string line;
+    map<int, vector <int> > list;
 
     inputFile.open("testinput.egonets");
     if(inputFile.is_open())
     {
         while(!inputFile.eof())
         {
+            // get the line from the file
             getline(inputFile, line);
-            stringstream ss(line); 
-            string num;
-            string temp;
-            int i;
-            bool first = true;
-
-            getline(ss, num, ' ');
-        
-            vector<string> nodes = tokenize(num, "\t");
-
-            cout << "FIRST " << nodes[0] << ": ";
-
-            for (int i = 1; i < nodes.size(); i++)
-                cout << nodes[i] << " ";
-            
-            cout << "endline" << endl;
+            // get the nodes on that line
+            vector <int> nodes = getInput(line);
+            // get the first node
+            int key = nodes[0];
+            //remove the first node from list of neighbors
+            nodes.erase(nodes.begin());
+            //assign neighbors to map with node as key
+            list[key] = nodes;
         }
-    }  
 
-    inputFile.close();
+        inputFile.close();
+
+    }
+
+    map<int, vector <int> >::iterator it;
+    for (it=list.begin(); it!=list.end(); ++it)
+    {
+        cout << "Node: " << it-> first << endl;
+        cout << "Neighbors: ";
+        vector<int> nodes = it -> second;
+
+         for (int i = 0; i < nodes.size(); i++)
+            {
+                cout << nodes[i] << " ";
+            }
+        cout << endl;
+    }
+        
+    /*
+    for (int i = 1; i < intNodes.size(); i++)
+    {
+        cout << intNodes[i] << " ";
+        if(intnodes[0] < intNodes[i])
+        {
+
+        }
+    }*/
+
 }
 
 
@@ -78,7 +120,7 @@ int main ()
 //take first element of row (u)
     //for each "neighbor" v of that row
         // if uv is less than v
-                //S = all neighbors of of first element, all neighbors of the selected neighbor (have to find them)
+                //S = all neighbors of first element, all neighbors of the selected neighbor (have to find them)
                 //add counts[1] += counts[1] + n - |S| -2
                 //for each element w in S
                     // if ( u < w || (v <w && w < u &&  v is not in the neighbor list of w)) // might have to modify this, because we are doing undirected graph, so we don't want to check different configurations, only one. maybe just u <w
