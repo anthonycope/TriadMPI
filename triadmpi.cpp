4bfd88b357468cd47b6bb8e8bb6fdb36dcf520fb
anthonycope <anthonycope@mail.usf.edu>
2013-09-29 22:33:27 -0400

    Finally got the file input working.

--- triadmpi.cpp
+++ triadmpi.cpp
@@ -1,12 +1,76 @@
 //MPI Triad Census Implementation
 //Anthony Cope and Joseph Fields
 
-// On RC, used module add compilers/gcc/4.4.7
+// On RC, use module add compilers/gcc/4.4.7
 //             module add mpi/openmpi/1.6.1
 // mpic++ -o triadmpi triadmpi.cpp
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
 #include "mpi.h"
+
+using namespace std;
+
+//function from: http://www.openframeworks.cc/files/005-rc/005-rc2-xcode-for-zach/addons/ofXmlSettings/src/ofXmlSettings.cpp
+vector<string> tokenize(const string & str, const string & delim)
+{
+  vector<string> tokens;
+  size_t p0 = 0, p1 = string::npos;
+
+  while(p0 != string::npos)
+  {
+    p1 = str.find_first_of(delim, p0);
+    if(p1 != p0)
+    {
+      string token = str.substr(p0, p1 - p0);
+      tokens.push_back(token);
+    }
+    p0 = str.find_first_not_of(delim, p1);
+ }
+return tokens;
+
+}
+
+int main ()
+{
+    fstream inputFile;
+    string line;
+
+    inputFile.open("testinput.egonets");
+    if(inputFile.is_open())
+    {
+        while(!inputFile.eof())
+        {
+            getline(inputFile, line);
+            stringstream ss(line); 
+            string num;
+            string temp;
+            int i;
+            bool first = true;
+
+            getline(ss, num, ' ');
+        
+            vector<string> nodes = tokenize(num, "\t");
+
+            cout << "FIRST " << nodes[0] << ": ";
+
+            for (int i = 1; i < nodes.size(); i++)
+                cout << nodes[i] << " ";
+            
+            cout << "endline" << endl;
+        }
+    }  
+
+    inputFile.close();
+}
+
+
 
 // int n = num nodes;
 //intialize an empty array to hold count for each type of triadmpi
