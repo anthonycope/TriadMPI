45beda199beb0f49879bb2c529a3b566d23be846
Joseph <jmfield2@mail.usf.edu>
2013-10-07 07:33:57 -0400

    initial mpi...kind of works

--- triadmpi.cpp
+++ triadmpi.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <iomanip>
 #include <stdlib.h>
 #include <vector>
 #include <map>
@@ -107,122 +108,230 @@ int tricode(int u, int v, int w) {
 
 	if (i == 63) return 3;
 
-printf("%d ", i);
-
 	// 0-63
 	return i;
 
 }
 
 
-int main ()
+int main (int argc, char *argv[])
 {
+    MPI_Init(&argc, &argv);
+    int mapSize, numProcessors, rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcessors);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+
     fstream inputFile;
     string line;
 
-    map<int, vector <int> > links;
-    int triadCounts[4] = {0, 0, 0, 0}, total = 0;
+    long double triadCounts[4] = {0, 0, 0, 0}, total = 0;
     int ret=0;
     int local1=0, local2=0;
     int count=0;
 
-    inputFile.open("testinput.egonets");
-    if(inputFile.is_open())
+   
+    if (rank == 0) {
+	    inputFile.open("testinput.egonets");
+	    if(inputFile.is_open())
+	    {
+	        while(!inputFile.eof())
+        	{
+	            // get the line from the file
+        	    getline(inputFile, line);
+	            // get the nodes on that line
+        	    vector <int> nodes = getInput(line);
+	            // get the first node
+        	    int key = nodes[0];
+	           //remove the first node from list of neighbors
+	            nodes.erase(nodes.begin());
+	
+		    sort(nodes.begin(), nodes.end());
+	
+	            //assign neighbors to map with node as key
+	            list[key] = nodes;
+		    count += nodes.size() + 1;
+	        }
+	
+	        inputFile.close();
+	
+	    }
+
+	    mapSize = list.size();
+	}
+
+    MPI_Bcast(&mapSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  
+    for (int i=0; i < mapSize; i++)  
     {
-        while(!inputFile.eof())
+	int rowSize, key;
+	vector<int> row;
+
+	if (rank == 0) {
+		row = list[i];
+		rowSize = row.size();
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Bcast(&rowSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	int rowArray[rowSize];
+
+	if (rank == 0) {
+		vector<int> vectorRow = list[i];
+		for (int x=0; x < rowSize; x++) {
+			rowArray[x] = vectorRow[x];
+		}
+		key = i;
+	}
+
+
+        MPI_Bcast(&rowArray, rowSize, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&key, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+
+       //links[i].assign(row, row.size());
+        if(rank != 0)
         {
-            // get the line from the file
-            getline(inputFile, line);
-            // get the nodes on that line
-            vector <int> nodes = getInput(line);
-            // get the first node
-            int key = nodes[0];
-            //remove the first node from list of neighbors
-            nodes.erase(nodes.begin());
+            vector<int> vectorRow;
+             for( int x = 0; x < rowSize; x++)
+            {
+               vectorRow.push_back(rowArray[x]);
+            }          
+           //cout << "Key: " << key << " | ";
+           list[key] = vectorRow;
 
-	    sort(nodes.begin(), nodes.end());
-
-            //assign neighbors to map with node as key
-            list[key] = nodes;
-	    count += nodes.size() + 1;
+           vector <int> testRow = list[key];/*
+           for( int j = 0; j < rowSize; j++)
+           {
+                //vectorRow = links[i];
+                cout << testRow[j] << " ";
+            }
+            cout << endl;*/
         }
 
-        inputFile.close();
-
     }
+		
 
     map<int, vector <int> >::iterator it;
 
-    for (it=list.begin(); it!=list.end(); ++it)
+   // had to modify loop so that each CPU would work on its own rows, and wouldn't overlap the others
+    for(int i = rank; i < mapSize; i+=numProcessors)
     {
-	// row u
-        int key = it -> first; 
-/*
-        cout << "Node: " << key << endl;
-        cout << "Neighbors: ";
-*/
-        vector<int> nodes = it -> second;
-
+        int u = i;
+        vector<int> nodes = list[i];    
 	// for each neighbor v
-        for (int i = 0; i < nodes.size(); i++)
-       	    {
-//                cout << nodes[i] << " ";
+        for (int x = 0; x < nodes.size(); x++)
+            {           
+                int v = nodes[x];                 
+                if(u < v) {
+                    vector <int> vNodes = list[v];
+                    
+                    //if(std::count(S.begin(), S.end(), key ) == 1)
+                      //  triadCounts[1] += 1; // every neighbor is 1 connection because it's undirected
+                   
+                    vector<int> S (nodes);
+                    vector<int> nodes2 = list[nodes[x]];
+                    sort(nodes2.begin(), nodes2.end());
 
-		if (key < nodes[i]) {
-			
-                    vector<int> S (nodes), S2 (nodes);
-                    vector<int> nodes2 = list[nodes[i]];
-                    vector<int> :: iterator it2;
-
-                    S2.insert(S2.end(), nodes2.begin(), nodes2.end() );
-                    sort(S2.begin(), S2.end());
-                    unique(S2.begin(), S2.end());
-		   
-		    if (link(key, nodes[i]) && link(nodes[i], key))
-			triadCounts[1] += list.size() - S2.size() - 2;
+                    // S is the union of the node and its neighbors
+                    S.insert(S.end(), nodes2.begin(), nodes2.end() );
+                    sort(S.begin(), S.end());
+                    unique(S.begin(), S.end());
+                    
+		    if (link(u, nodes[i]) && link(nodes[i], u))
+			triadCounts[1] += list.size() - S.size() - 2;
 		    else triadCounts[2] += list.size() - S.size() - 2;
+   		    // XXX remove u,v
 
 		    local1 = 0;
-	
-		    for (int j=0; j < S.size(); j++) {
-				if (nodes[i] < S[j] || 
-				    (key < S[j] && S[j] < nodes[i] && !link(S[j], key)) ) {
+      		    for (int y=0; y < vNodes.size(); y++) 
+                    {
+                        int w = vNodes[y];     
+                        triadCounts[2] +=1; // if its in nodes, it's already connected once.  every neighbor of this neighbor then forms a 2 connection triad.                   
+			if (nodes[i] < w || 
+			    (u < w && w < nodes[i] && !link(w, u)) ) {
+                        {           
+				ret = tricode(u, nodes[i], w);
 
-					ret = tricode(key, nodes[i], S[j]);
-
-					triadCounts[ret]++;
-					local1++;
-				}
+				triadCounts[ret]++;
+				local1++;
+			}
 	   	    }
 
                    if (S.size() - local1 > 0) triadCounts[2] += S.size() - local1;
                    else if (S.size() <= 0) triadCounts[1]++;
+                            
+                }   
+                //cout << endl;
+        }
+    }
+   }
 
-		}
+   if(rank != 0)
+    {
+        MPI_Send(&triadCounts,4,MPI_LONG_DOUBLE, 0, 1, MPI_COMM_WORLD);
+        //cout << "SENT" << endl;
+    }
+    if(rank == 0)
+    {
+        int count = 0;
+        long double counts[4] = {0,0,0,0};
+        
+        while(count < numProcessors -1) // so all processes return
+        {
+            MPI_Recv(&counts, 4, MPI_LONG_DOUBLE, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            for(int x = 0; x < 4; x++)
+            {
+                triadCounts[x] += counts[x];
+            }
+            count++;
+        }
+
+        //cout << "HERE" << endl;             
+
+        for (int i = 1; i < 4; i++) 
+        {
+     		total += triadCounts[i];		
+        }
+
+    }
+   
+    if (rank == 0) {
+        //long double numZero =  (long double) 12477843 * (long double)(12477843 -1) * (long double)(12477843 -2)/6.0;
+        long double numZero =  (long double) mapSize * (long double)(mapSize -1) * (long double) (mapSize -2)/6.0;
+        cout.setf(ios::fixed);            
+        cout <<  "NumZero: " << setprecision(0) << numZero << endl;
+
+        numZero-= total;
+        triadCounts[0] = (long double) numZero;
+        total += triadCounts[0];
+
+        cout << endl << "Triad Counts: " << endl;
+
+        for (int i = 0; i < 4; i++) 
+        {
+            cout.setf(ios::fixed);
+            cout << setprecision(0) << i << ": " << triadCounts[i] << endl;
         }
 
 
+        cout << "Count: " << mapSize << endl;
+        cout << "Total: " << total << endl;
+
+        cout << endl;
     }
 
-    total = triadCounts[1] + triadCounts[2] + triadCounts[3];
+    MPI_Finalize();
+
+    return 0;
+}
+
 
     // n! / r!(n-r)!
     // or 1000! / 3! * 997! = 1000*999*998 / 6
     // 166167000
 
-    triadCounts[0] = (list.size() * (list.size()-1) * (list.size()-2))/6 - total;
-
-    total += triadCounts[0];
-
-    cout << endl << "Triad Counts: " << endl;
-    for (int i = 0; i < 4; i++) {
-		cout << i << ": " << triadCounts[i] << endl;
-    }
-    cout << "Total: " << total << endl;
-
-    cout << endl;
-
-}
 
 // int n = num nodes;
 //intialize an empty array to hold count for each type of triadmpi
